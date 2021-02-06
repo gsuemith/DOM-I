c9660942240f90fa6b2e687f750bca8dc43f6dfa
Garrick Suemith <garrick.suemith@gmail.com>
2021-02-06 11:21:05 -0500

    selected seconds

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,6 @@
+let cs = document.getElementById('msTens');
+let ds = document.getElementById('msHundreds');
+let s = document.getElementById('secondOnes');
+let Ds = document.getElementById('secondTens');
+
+console.log(cs.textContent);
